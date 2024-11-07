6cea2437eb60799ac911a41e2579d705228f46ea
RABUNTHOEUN <rabunthoeun7777@gmail.com>
2024-11-07 08:54:30 +0700

    update

--- components/my-header.tsx
+++ components/my-header.tsx
@@ -1,4 +1,4 @@
-import { Menu, ShoppingCart, User2Icon } from "lucide-react";
+import { Menu, Search, SearchCheckIcon, ShoppingCart, User2Icon } from "lucide-react";
 import {
     Sheet,
     SheetContent,
@@ -13,55 +13,72 @@ import { Button } from "@/components/ui/button";
 
 const MyHeader = () => {
     return (
-        <div className="bg-primary ">
-            <header className="flex px-2 max-w-screen-2xl items-center justify-between mx-auto">
-                <div className="flex items-center">
-                    <Sheet>
-                        <SheetTrigger>
-                            <Menu className="size-8 text-white" />
-                        </SheetTrigger>
-                        <SheetContent side='left'>
-                            <SheetHeader>
-                                <SheetTitle>Menu</SheetTitle>
-                            </SheetHeader>
-                            <ul className="flex flex-col gap-8">
-                                <li>Laptops</li>
-                                <li>Desktops</li>
-                                <li>Networking Devices</li>
-                                <li>Laptops</li>
-                                <li>Desktops</li>
-                                <li>Networking Devices</li>
-                                <li>Laptops</li>
-                                <li>Desktops</li>
-                                <li>Networking Devices</li>
-                            </ul>
-                        </SheetContent>
-                    </Sheet>
-                    <img src="/images/logo/logo.png" alt="" className="w-20" />
+        <>
+            <div className="bg-primary text-white">
+                <div className="flex justify-between items-center py-4 max-w-screen-2xl mx-auto text-[12px] text-gray-400">
+                    <div>Mon-Thu: <span className="font-semibold text-white">9:00-5:30 PM ▼</span></div>
+                    <div>Visit our showroom in 1234 Street Adress City Address, 1234  <span className="font-semibold text-white underline">Contact Us</span></div>
+                    <div className="flex gap-4 font-semibold text-white">
+                        <p>Call Us:(00)1234 5678</p>
+                        <Image width={20} height={20} src="/icons/fb.png" alt="" />
+                        <Image width={20} height={20} src="/icons/ig.png" alt="" />
+                    </div>
                 </div>
-                <div className="hidden lg:flex flex-1">
-                    <ul className="flex gap-8 text-white">
-                        <li>Laptops</li>
-                        <li>Desktops</li>
-                        <li>Networking Devices</li>
-                        <li>Laptops</li>
-                        <li>Desktops</li>
-                        <li>Networking Devices</li>
-                        <li>Laptops</li>
-                        <li>Desktops</li>
-                        <li>Networking Devices</li>
-                    </ul>
-                </div>
-                <div className="flex gap-4">
-                    <Button size='icon' variant='ghost'>
-                        <ShoppingCart className="text-white" />
-                    </Button>
-                    <Button size='icon' variant='ghost'>
-                        <User2Icon className="text-white" />
-                    </Button>
-                </div>
-            </header>
-        </div>
+            </div>
+            <div className="bg-background">
+                <header className="flex px-2 max-w-screen-2xl items-center justify-between mx-auto py-4">
+                    <div className="flex items-center">
+                        <Sheet>
+                            <SheetTrigger>
+                                <Menu className="size-8 text-white lg:hidden" />
+                            </SheetTrigger>
+                            <SheetContent side='left'>
+                                <SheetHeader>
+                                    <SheetTitle>Menu</SheetTitle>
+                                </SheetHeader>
+                                <ul className="flex flex-col gap-8 font-semibold">
+                                    <li>Laptops</li>
+                                    <li>Desktops</li>
+                                    <li>Networking Devices</li>
+                                    <li>Laptops</li>
+                                    <li>Desktops</li>
+                                    <li>Networking Devices</li>
+                                    <li>Laptops</li>
+                                    <li>Desktops</li>
+                                    <li>Networking Devices</li>
+                                </ul>
+                            </SheetContent>
+                        </Sheet>
+                        <img src="/images/logo/logo.png" alt="" className="w-20" />
+                    </div>
+                    <div className="hidden lg:flex flex-1 justify-evenly">
+                        <ul className="flex gap-8 font-semibold">
+                            <li>Laptops</li>
+                            <li>Desktops</li>
+                            <li>Networking Devices</li>
+                            <li>Laptops</li>
+                            <li>Desktops</li>
+                            <li>Networking Devices</li>
+                            <li>Laptops</li>
+                            <li>Desktops</li>
+                            <li>Networking Devices</li>
+                        </ul>
+                    </div>
+                    <div className="flex gap-4">
+                        <Button size='icon' variant='ghost'>
+                            <Search className="" />
+                        </Button>
+                        <Button size='icon' variant='ghost'>
+                            <ShoppingCart className="" />
+                        </Button>
+                        <Button size='icon' variant='ghost'>
+                            <User2Icon className="" />
+                        </Button>
+                    </div>
+                </header>
+                <hr />
+            </div>
+        </>
     )
 }
 
