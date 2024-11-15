cca042600ed729b8bb5e847e3c8753db683cdde2
rabunthoeun7777 <rabunthoeun7777@gmail.com>
2024-11-15 20:52:17 +0700

    register and login form

---
+++ app/(client)/(auth)/login/page.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+
+    return (
+        <div>
+            <p>Home  ›  Login</p>
+            <h1 className='text-3xl font-semibold my-8'>Customer Login</h1>
+            <div className='flex justify-center gap-8 min-w-full mb-8'>
+                <div className='bg-blue-50 p-8 rounded-md w-1/3'>
+                    <h3 className='font-semibold text-xl'>Login Customers</h3>
+                    <p className='text-sm my-4'>If you have an account, sign in with your email address.</p>
+                    <form action="">
+                        <div className='flex flex-col w-full pb-4'>
+                            <label htmlFor="forName" >Your Email</label>
+                            <input className='border px-4 py-2 rounded-md ' type="email" placeholder='Your Email' required />
+                        </div>
+                        <div className='flex flex-col w-full pb-4'>
+                            <label htmlFor="forEmail">Your Password</label>
+                            <input className='border px-4 py-2 rounded-md' type="password" placeholder='Your Password' required />
+                        </div>
+
+                        <div className='flex gap-8 my-4 items-center'>
+                            <button className='px-8 py-2 border-2 font-semibold rounded-full text-white bg-blue-600 border-blue-600 hover:bg-white hover:text-blue-600 transition' type="submit">Login</button>
+                            <Link href="/forgot-password" className='text-sm font-semibold text-blue-500 hover:underline'>
+                                Forgot Your Password?
+                            </Link>
+                        </div>
+                    </form>
+                </div>
+                <div className='bg-blue-50 p-8 rounded-md w-1/3'>
+                    <h3 className='font-semibold text-xl'>New Customer?</h3>
+                    <p className='text-sm my-4'>Creating an account has many benefits: </p>
+                    <ul className='text-sm text-gray-500'>
+                        <li>•  Check out faster</li>
+                        <li>•  Keep more than one address</li>
+                        <li>•  Track orders and more</li>
+                    </ul>
+                    <Link href="/register">
+                        <button className='my-8 px-8 py-2 border-2 font-semibold rounded-full text-white bg-blue-600 border-blue-600 hover:bg-white hover:text-blue-600 transition' type="submit">Create An Account</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
